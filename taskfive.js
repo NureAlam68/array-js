62f7c537e2b1d1667041054bbc6aacca7dae4ede
NureAlam68 <nurealam151068@gmail.com>
2024-08-31 21:36:51 +0600

    added task five

---
+++ taskfive.js
@@ -0,0 +1,21 @@
+// Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+
+
+
+let firstName = ["Nure", "Sn", "Saymon"];
+
+let lastName = ["Alam", "Sohag", "Gazi"];
+
+firstName.concat(lastName);
+
+console.log(firstName);
+
+console.log(lastName);
+
+console.log(firstName.concat(lastName));
