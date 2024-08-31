b15f9b192136d8eedfaecb9031546628df67c910
NureAlam68 <nurealam151068@gmail.com>
2024-08-31 21:01:18 +0600

    added task two

---
+++ tasktwo.js
@@ -0,0 +1,17 @@
+// 2. Add or remove elements
+
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+let place = ["London", "Istanbul", "Rome"];
+
+place.push("Sydney");
+
+place.push("Tokyo", "Paris");
+
+place.pop();
+
+console.log(place);
