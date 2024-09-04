f25d9f904e9ba8ac6c49897595692e7563812c12
NureAlam68 <nurealam151068@gmail.com>
2024-09-04 13:06:07 +0600

    sort js

---
+++ sort.js
@@ -0,0 +1,64 @@
+// .sort() diye sajano jay ascending and descending kora jay tobe akto jamela ce seta solve kora jay
+
+
+
+// let persons = ['rakib', 'akib', 'dakib', 'nokib', 'bakib']
+
+// let sortedPersons = persons.sort();
+
+// console.log(sortedPersons);
+
+
+
+
+// let persons = ['Rakib', 'akib', 'Dakib', 'nokib', 'bakib']
+
+// let sortedPersons = persons.sort();
+
+// console.log(sortedPersons);
+
+
+
+// let numbers = [1, 4, 5, 7, 2, 3, 9,]
+
+// let sortedNumbers = numbers.sort();
+
+// console.log(sortedNumbers);
+
+
+
+// let numbers = [1, 44, 5, 17, 22, 3, 9,]
+
+// let sortedNumbers = numbers.sort();
+
+// console.log(sortedNumbers);
+
+
+
+
+// solve problem
+
+
+// 1. Ascending
+
+
+// let numbers = [1,14, 5, 77, 2, 3, 39, 7, 8, 9,];
+
+// let sortedNumbers = [...numbers].sort(function(a,b){return a - b});
+
+// console.log(sortedNumbers);
+
+
+
+
+
+
+
+// 1. Descending
+
+
+let numbers = [1,14, 5, 77, 2, 3, 39, 7, 8, 9,];
+
+let sortedNumbers = [...numbers].sort(function(a,b){return b - a});
+
+console.log(sortedNumbers);
