9a69f3eb3efe5a0414f3b7fd612fdf220c7e3a47
NureAlam68 <nurealam151068@gmail.com>
2024-09-04 12:20:44 +0600

    looping array

---
+++ looping.js
@@ -0,0 +1,74 @@
+// loop in array
+
+
+
+
+// for of loop
+
+
+// let friends = ['Elon', 'Mark', 'Bill'];
+
+// for(let friend of friends) {
+//     console.log(friend);
+// }
+// console.log(friends.length) for know how many component is present in this Array
+
+
+
+
+// for loop
+
+// let friends = ['Elon', 'Mark', 'Bill'];
+
+// for(let i = 0; i < friends.length; i++) {
+//     console.log(friends[i]);
+// }
+
+
+
+// while loop
+
+// let friends = ['Elon', 'Mark', 'Bill'];
+
+// let i = 0;
+
+// while(i < friends.length) {
+//     console.log(friends[i]);
+//     i++;
+// }
+
+
+
+
+// number array
+
+
+// for of
+
+// let numbers = [11, 22, 33, 4, 5, 6, 7, 99];
+
+// for(let num of numbers) {
+//     console.log(num);
+// }
+
+
+
+// for 
+
+// let numbers = [11, 22, 33, 4, 5, 6, 7, 99];
+
+// for(let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+
+// while
+
+let numbers = [11, 22, 33, 4, 5, 6, 7, 99];
+
+let i = 0;
+
+while(i < numbers.length) {
+    console.log(numbers[i]);
+    i++;
+}
